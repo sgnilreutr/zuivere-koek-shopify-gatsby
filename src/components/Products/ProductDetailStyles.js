7d751dda8218199e55b76661f2537d8832dd28fa
Robbert Tuerlings <robberttg@protonmail.com>
2021-04-06 21:27:46 +0200

    Initial setup

---
+++ src/components/Products/ProductDetailStyles.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+
+export const ProductWrapper = styled.div`
+  margin-top: 10%;
+  background-color: hsl(351, 81%, 94%);
+  display: flex;
+  flex-flow: row;
+  padding: 2rem;
+`
+
+export const ProductImage = styled.div`
+  width: 307px;
+`
+
+export const ProductDesc = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin-left: 2%;
+`
+
+export const AddToCart = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+`
