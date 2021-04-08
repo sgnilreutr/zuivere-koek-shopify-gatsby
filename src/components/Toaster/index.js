23b15389762550076ccd5c45dbefa9d8f8515cb2
Robbert Tuerlings <robberttg@protonmail.com>
2021-04-08 16:49:47 +0200

    Update Toaster

--- src/components/Toaster/index.js
+++ src/components/Toaster/index.js
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const Toaster = () => {
-  return <ToastContainer pauseOnFocusLoss={false} autoClose={3000} hideProgressBar/>
+    return <ToastContainer pauseOnFocusLoss={false} autoClose={3000} hideProgressBar />
 }
 
 export default Toaster
