4d1db22f5d7c2e7acc539cbff367046d72fbbac3
Robbert Tuerlings <robberttg@protonmail.com>
2021-04-29 19:15:58 +0200

    Update

--- src/components/Homepage/PeopleStyles.js
+++ src/components/Homepage/PeopleStyles.js
@@ -20,19 +20,19 @@ export const ContentGrid = styled.div`
   transform: translateY(-3rem);
   grid-template-columns: auto auto;
   /* grid-template-columns: repeat(auto-fill, minmax(780px, 1fr));; */
-  @media only screen and (max-width: 767px) {
+  @media only screen and (max-width: 1023px) {
     grid-template-columns: none;
     transform: none;
   }
 `
 
 export const ImageContainer = styled.figure`
-  width: 780px;
-  height: 460px;
+  max-width: 780px;
+  max-height: 460px;
+  margin: 0;
   @media only screen and (max-width: 767px) {
     width: 100%;
     height: auto;
-    margin: 0;
     display: grid;
   }
   /* margin-right: 6rem; */
@@ -43,7 +43,7 @@ export const TextContainer = styled.div`
   height: 440px;
   padding: 3rem 2rem 2rem;
   background-color: hsl(351, 81%, 94%);
-  @media only screen and (max-width: 767px) {
+  @media only screen and (max-width: 1023px) {
     width: 100%;
     height: auto;
     display: grid;
